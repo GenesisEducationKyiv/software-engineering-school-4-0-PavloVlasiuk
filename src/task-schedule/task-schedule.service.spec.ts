ceead4b42054b408cd6e4e9c97c2a4ae4af69575
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-06-16 00:33:38 +0300

    feat(tests): implement tests for task schedule service

---
+++ src/task-schedule/task-schedule.service.spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { TaskScheduleService } from './task-schedule.service';
+import { EmailService } from '../email/services/email.service';
+import { IExchangeRate } from '../rate/interfaces';
+import { RateService } from '../rate/rate.service';
+
+describe('TaskScheduleService', () => {
+  let taskScheduleService: TaskScheduleService;
+  let rateService: RateService;
+  let emailService: EmailService;
+
+  beforeEach(async () => {
+    const testingModule: TestingModule = await Test.createTestingModule({
+      providers: [
+        TaskScheduleService,
+        {
+          provide: RateService,
+          useValue: {
+            getCurrentRate: jest.fn(),
+          },
+        },
+        {
+          provide: EmailService,
+          useValue: {
+            sendCurrentRate: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    taskScheduleService =
+      testingModule.get<TaskScheduleService>(TaskScheduleService);
+    rateService = testingModule.get<RateService>(RateService);
+    emailService = testingModule.get<EmailService>(EmailService);
+  });
+
+  it('should be defined', () => {
+    expect(taskScheduleService).toBeDefined();
+  });
+
+  describe('sendCurrentRate', () => {
+    it('should send current rate to subscribers', async () => {
+      const currency: IExchangeRate = {
+        rate: 39.414,
+        exchangedate: '16.06.2024',
+      };
+
+      jest.spyOn(rateService, 'getCurrentRate').mockResolvedValue(currency);
+
+      jest.spyOn(emailService, 'sendCurrentRate').mockResolvedValue(undefined);
+
+      const response = await taskScheduleService.sendCurrentRateEmail();
+
+      expect(response).toBeUndefined();
+
+      expect(rateService.getCurrentRate).toHaveBeenCalledTimes(1);
+
+      expect(emailService.sendCurrentRate).toHaveBeenCalledTimes(1);
+    });
+  });
+});
