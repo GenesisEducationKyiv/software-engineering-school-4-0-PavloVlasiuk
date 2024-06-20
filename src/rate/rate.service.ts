caa2af2b386a523a3d7c0a8ece24815fa55af7b4
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-06-20 17:15:40 +0300

    feat: add handling errors from exchange rate api

--- src/rate/rate.service.ts
+++ src/rate/rate.service.ts
@@ -1,7 +1,8 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom } from 'rxjs';
 
+import { ExchangeRateAPIException } from './exceptions';
 import { IExchangeRate, IGetExchangeRate } from './interfaces';
 import { API_URL } from './rate.constants';
 
@@ -13,7 +14,12 @@ export class RateService {
     const {
       data: [currency],
     } = await firstValueFrom(
-      this.httpService.get<Array<IGetExchangeRate>>(API_URL),
+      this.httpService.get<Array<IGetExchangeRate>>(API_URL).pipe(
+        catchError((e) => {
+          console.error(e);
+          throw new ExchangeRateAPIException();
+        }),
+      ),
     );
 
     return {
