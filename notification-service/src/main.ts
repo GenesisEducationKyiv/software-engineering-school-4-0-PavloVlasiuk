1e795a35f186dc7118689611824c21c6359caf54
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-07-11 00:01:48 +0300

    feat: implement basic communication between subscription and notification services via rabbitmq

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,10 +1,7 @@
-import { join } from 'path';
-
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
-import { NOTIFICATION_PACKAGE_NAME } from '../../proto/dist/types/notification';
 
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
@@ -16,14 +13,10 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.GRPC,
+      transport: Transport.RMQ,
       options: {
-        package: NOTIFICATION_PACKAGE_NAME,
-        protoPath: join(
-          __dirname,
-          '../../../../proto/notification/notification.proto',
-        ),
-        url: `${process.env.HOST}:${process.env.PORT}`,
+        urls: [process.env.RABBITMQ_URL],
+        queue: process.env.RABBITMQ_QUEUE,
       },
     },
   );
