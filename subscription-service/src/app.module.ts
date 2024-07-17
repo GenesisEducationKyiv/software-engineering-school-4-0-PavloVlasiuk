b048147d31d64a5ba7cc83be02d8df79684a24f1
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-07-17 13:02:21 +0300

    feat: create factory to instantiate events and implement producing and consuming of such events

--- subscription-service/src/app.module.ts
+++ subscription-service/src/app.module.ts
@@ -13,7 +13,6 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from './database/database.module';
 import { RateModule } from './rate/rate.module';
 import { SubscriptionModule } from './subscription/subscription.module';
-import { TaskScheduleModule } from './task-schedule/task-schedule.module';
 
 @Module({
   imports: [
