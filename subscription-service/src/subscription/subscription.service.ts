60e676c542b1b3ca799ea5987c899b1ce6e83b31
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-07-18 16:11:08 +0300

    feat: create unsubscribe endpoint

--- subscription-service/src/subscription/subscription.service.ts
+++ subscription-service/src/subscription/subscription.service.ts
@@ -1,8 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common';
 
-import { SubscribeEmailRequestDto } from './dto/requests';
+import {
+  SubscribeEmailRequestDto,
+  UnsubscribeEmailRequestDto,
+} from './dto/requests';
 import { Subscription } from './entities';
-import { AlreadySubscribedException } from './exceptions';
+import {
+  AlreadySubscribedException,
+  EntityNotFoundException,
+} from './exceptions';
 import {
   ISubscriptionRepository,
   SUBSCRIPTION_REPOSITORY,
@@ -17,15 +23,26 @@ export class SubscriptionService implements ISubscriptionService {
   ) {}
 
   async subscribe({ email }: SubscribeEmailRequestDto): Promise<void> {
-    const alreadySubscribed =
-      await this.subscriptionRepository.findByEmail(email);
+    const alreadySubscribed = await this.isSubscribed(email);
 
     if (alreadySubscribed) throw new AlreadySubscribedException();
 
-    await this.subscriptionRepository.create(email);
+    await this.subscriptionRepository.createOrUpdate(email);
   }
 
   async getAllSubscribers(): Promise<Subscription[]> {
     return this.subscriptionRepository.findAll();
+  }
+
+  async unsubscribe({ email }: UnsubscribeEmailRequestDto): Promise<void> {
+    const isSubscribed = await this.isSubscribed(email);
+
+    if (!isSubscribed) throw new EntityNotFoundException('Subscription');
+
+    await this.subscriptionRepository.deleteByEmail(email);
+  }
+
+  private async isSubscribed(email: string): Promise<Subscription | null> {
+    return this.subscriptionRepository.findByEmail(email);
   }
 }
