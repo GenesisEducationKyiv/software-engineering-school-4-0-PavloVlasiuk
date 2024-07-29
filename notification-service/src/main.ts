eb791f942d8e28c54faa5497f84a6bd52c0b412e
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-07-29 17:11:10 +0300

    feat(notification-service): implement rate module with rabbitmq controller

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -5,24 +5,36 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './exceptions/filters';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: [process.env.RABBITMQ_URL],
-        queue: 'subscription_queue',
-        noAck: false,
-        persistent: true,
-        queueOptions: {
-          durable: true,
-        },
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL],
+      queue: 'subscription_queue',
+      noAck: false,
+      persistent: true,
+      queueOptions: {
+        durable: true,
       },
     },
-  );
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL],
+      queue: 'rate_queue',
+      noAck: false,
+      persistent: true,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
 
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  await app.listen();
+  await app.startAllMicroservices();
 }
 bootstrap();
---
+++ notification-service/src/rate/rate.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { UpdateRateDto } from './dto';
+import { IRateService } from './interfaces';
+import { Rate, RateDocument } from './schemas/rate.schema';
+
+@Injectable()
+export class RateService implements IRateService {
+  constructor(
+    @InjectModel(Rate.name)
+    private readonly rateModel: Model<RateDocument>,
+  ) {}
+
+  async createCurrent({ rate, exchangeDate }: UpdateRateDto) {
+    const newRate = new this.rateModel({ value: rate, exchangeDate });
+
+    await newRate.save();
+  }
+
+  async getLatest(): Promise<Rate> {
+    return this.rateModel.findOne().sort({ createdAt: -1 }).exec();
+  }
+}
