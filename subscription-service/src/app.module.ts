6e48875c82e2492dd196679d6cf04f5b586a0ee1
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-07-30 16:27:50 +0300

    refactor(subscription-service): delete unnecessary rate module

--- subscription-service/src/app.module.ts
+++ subscription-service/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from './database/database.module';
 import { EventModule } from './event/event.module';
-import { RateModule } from './rate/rate.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { RateModule } from './rate/rate.module';
 import { TaskScheduleModule } from './task-schedule/task-schedule.module';
@@ -13,6 +12,6 @@ import { RateModule } from './rate/rate.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [DatabaseModule, SubscriptionModule, RateModule, EventModule],
+  imports: [DatabaseModule, SubscriptionModule, EventModule],
 })
 export class AppModule {}
