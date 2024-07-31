2d1bc6366f3ca31628ffd39d8683f6cb60ab22ad
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-07-31 03:12:05 +0300

    refactor(notification-service): create rabbitmq configs and delete hardcoded values

--- notification-service/src/config/index.ts
+++ notification-service/src/config/index.ts
@@ -1,4 +1,5 @@
 import emailConfig from './email.config';
 import mongodbConfig from './mongodb.config';
+import rabbitmqConfig from './rabbitmq.config';
 
-export default [emailConfig, mongodbConfig];
+export default [emailConfig, mongodbConfig, rabbitmqConfig];
