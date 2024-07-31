c2b7588b24f5036b3220f60a86facdf79682db29
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2024-07-31 03:12:05 +0300

    refactor(notification-service): create rabbitmq configs and delete hardcoded values

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -2,36 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
+import { AppConfigService } from './config/app-config';
 import { AllExceptionsFilter } from './exceptions/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBITMQ_URL],
-      queue: 'subscription_queue',
-      noAck: false,
-      persistent: true,
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
+  const config = app.get<AppConfigService>(AppConfigService);
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBITMQ_URL],
-      queue: 'rate_queue',
-      noAck: false,
-      persistent: true,
-      queueOptions: {
-        durable: true,
+  const queues = config.get<{ [K: string]: string }>('rabbitmq.queues');
+
+  for (const queue of Object.values(queues)) {
+    app.connectMicroservice<MicroserviceOptions>({
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue,
+        noAck: false,
+        persistent: true,
+        queueOptions: {
+          durable: true,
+        },
       },
-    },
-  });
+    });
+  }
 
   app.useGlobalFilters(new AllExceptionsFilter());
 
